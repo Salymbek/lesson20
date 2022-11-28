b30917d73dcfd6951bcc7078c487f8b24d72d1ca
Salymbek <calumbekcaatbekov@gmail.com>
2022-11-28 17:42:03 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+
+
+            Scanner scanner = new Scanner(System.in);
+
+
+            Parallelepiped parallelepiped = new Parallelepiped();
+            System.out.println("     HRIGTH    :");
+            parallelepiped.setHeigth(scanner.nextInt());
+            System.out.println("     LENGTH    :");
+            parallelepiped.setLength(scanner.nextInt());
+            System.out.println("     WIDTH     :");
+            parallelepiped.setWidth(scanner.nextInt());
+
+
+            if (parallelepiped.getHeigth() < 0 || parallelepiped.getLength() < 0 || parallelepiped.getWidth() < 0) {
+                throw new ArithmeticException("this number is negative");
+
+            }
+            if (parallelepiped.getHeigth() > 20 || parallelepiped.getLength() > 20 || parallelepiped.getWidth() > 20) {
+
+                throw new ArithmeticException("this number is greater than 20");
+            }
+
+
+
+            System.out.println("                     SQUARE ");
+
+            System.out.println(parallelepiped.getSquare(parallelepiped.getHeigth(), parallelepiped.getLength(), parallelepiped.getWidth()));
+
+
+            System.out.println("                     VOLUME");
+
+            System.out.println(parallelepiped.getVolume(parallelepiped.getHeigth(), parallelepiped.getLength(), parallelepiped.getWidth()));
+
+
+
+
+
     }
 }
--- src/Parallelepiped.java
+++ src/Parallelepiped.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class Parallelepiped {
+public class Parallelepiped {
+    private int heigth;
+    private int length;
+    private int width;
+
+
+    public int getSquare(int heigth, int length, int width){
+        return 2 *((heigth * length) + (length * width) + (heigth * width));
+    }
+
+
+    public int getVolume(int heigth, int length, int width){
+        return  (length * width * heigth);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int getHeigth() {
+        return heigth;
+    }
+
+    public void setHeigth(int heigth) {
+        this.heigth = heigth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+
+
+    public Parallelepiped(int heigth, int length, int width) {
+        this.heigth = heigth;
+        this.length = length;
+        this.width = width;
+    }
+
+
+    public Parallelepiped() {
+    }
+
+
+
 }
